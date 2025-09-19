c6039a74974db9fea103f860b5f27dbe1f187511
huangzhenming <huangzhenming@qunjielong.com>
2025-09-20 00:57:27 +0800

    feat: release script
    Signed-off-by: huangzhenming <huangzhenming@qunjielong.com>

---
+++ release.bat
@@ -0,0 +1,28 @@
+@echo off
+REM ---------------------------------------------------------
+REM Build script for current project with CGO_ENABLED=1
+REM ---------------------------------------------------------
+
+REM 切换到当前批处理所在目录
+cd /d %~dp0
+
+REM 打开 CGO 支持
+set CGO_ENABLED=1
+
+REM 如果需要可以指定 gcc 路径（可选）
+REM set CC=D:\mingw64\bin\gcc.exe
+REM set CXX=D:\mingw64\bin\g++.exe
+
+REM 清理 Go 缓存（可选）
+REM go clean -cache
+
+REM 编译当前目录
+@REM go build -o app.exe main.go
+fyne package --name Fers --icon Icon.jpg --os windows --release --id fers.mingregister.github.com 
+
+REM 提示完成
+echo.
+echo ============================
+echo Build completed: app.exe
+echo ============================
+pause
